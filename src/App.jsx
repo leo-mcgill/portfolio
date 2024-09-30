d5e3fa854e02f3975bbfdb206bf532bf6cee8b99
leo-mcgill <leomcgill1@icloud.com>
2024-09-30 22:39:49 +0800

    fixed ascii art

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import Sidebar from "./Components/Sidebar"
+import Sidebar from "./Components/Sidebar";
+import { useAsciiText, ansiShadow } from 'react-ascii-text';
+
+function AsciiArt() {
+    const asciiTextRef = useAsciiText({
+        font: ansiShadow,
+        text: "L-E-O",
+    });
+
+    return <pre className='ascii-art'>
+{`
+██╗                 ███████╗     ██████╗              ███╗          ███╗ ██████╗ ██████╗ ██╗██╗            ██╗     
+██║                 ██╔════╝   ██╔═══██╗          ████╗   ████║ ██╔═══╝ ██╔═══╝ ██║██║            ██║
+██║                 █████╗         ██║          ██║          ██╔████╔██║ ██║              ██║███╗ ██║██║            ██║     
+██║                 ██╔══╝         ██║          ██║          ██║╚██╔╝██║ ██║              ██║   ██║ ██║██║            ██║     
+███████╗ ███████╗  ╚██████╔╝           ██║  ╚═╝    ██║ ██████╗ ██████╝ ██║█████╗  ██████╗
+╚══════╝ ╚══════╝    ╚═════╝              ╚═╝                ╚═╝ ╚═════╝ ╚═════╝ ╚═╝╚════╝   ╚═════╝
+`}
+    </pre>
+}
 
 export default function App(){
-  return (
-  <>
-  <Router>
-      <div className='App'>
-        <div className='terminal-screen'>
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+    const [startScreen, setStartScreen] = useState(true);
+    useEffect(() => {
+        const handleKeyPress = (event) => {
+          if (event.code === 'Enter') {
+            setStartScreen(false); // Hide start screen when space is pressed
+          }
+        };
+    
+        window.addEventListener('keydown', handleKeyPress);
+    
+        // Cleanup event listener
+        return () => {
+          window.removeEventListener('keydown', handleKeyPress);
+        };
+      }, []);
+
+
+    return (
+    <>
+    <Router>
+        <div className='App'>
+            {startScreen ? (
+            <div className="start-screen">
+            <AsciiArt />
+            <p>start &lt;Enter&gt;</p>
+            </div>
+        ) : (        
+            <div className='terminal-screen'>
+            <Sidebar />
+            <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            </Routes>
+            </div>
+            )}
         </div>
-      </div>
     </Router>
-  </>
-  );
+    </>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { useAsciiText, ansiShadow } from 'react-ascii-text';
 
-function MyComponent() {
-    const asciiTextRef = useAsciiText({
-        font: ansiShadow,
-        text: "Leo McGill",
-    });
-
-    return <pre className='ascii-art' ref={asciiTextRef}></pre>;
-}
 
 function Home(){
     return (
     <>
-    <div>
-    <MyComponent />
+    <div className='homeScreen'>
+        <p> This is home page </p>
     </div>
     </>
     );
